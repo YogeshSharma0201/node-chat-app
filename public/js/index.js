2f1e9d265b8126044952ffe358f4e9d6f208a87e
yogeshSharma0201 <yogesh02011998@gmail.com>
2017-07-09 18:13:32 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -27,12 +27,12 @@ socket.on('newLocationMessage', function(message){
 
 $('#message-form').on('submit', function(e){
   e.preventDefault();
-
+  var messageTextbox = $('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextbox.val()
   }, function(){
-
+    messageTextbox.val('');
   });
 });
 
@@ -41,13 +41,15 @@ locationButton.on('click', function () {
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
-
+  locationButton.attr('disabled', 'disabled').text('Sending Location...');
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function(){
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location.');
   })
 });
